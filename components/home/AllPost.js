a87cefd5cf55e0f0662c6b2e4c748cca3425e4c2
IntDipto <dipto.dey@intelli.global>
2023-01-05 11:34:09 +0600

    home all post bug fixed

--- components/home/AllPost.js
+++ components/home/AllPost.js
@@ -43,7 +43,7 @@ function AllPost() {
     </div>
     <div className="mt-10  mx-6">
         <ResponsiveMasonry
-            columnscountBreakPoints={{350: 1, 550:2, 900: 3, 1200:4}}
+            columnsCountBreakPoints={{350: 1, 550:2, 900: 3, 1200:4}}
         >
             <Masonry> 
                 {
