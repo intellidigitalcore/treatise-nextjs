1900091d3d64af7e0622938d59704f598b7a3584
IntDipto <dipto.dey@intelli.global>
2023-01-11 13:55:36 +0600

    minors bugs

--- components/shared-components/Footer.js
+++ components/shared-components/Footer.js
@@ -63,6 +63,7 @@ function Footer() {
                             </svg>
                         </button>
                     </li>
+
                     <li>
                         <button>
                             <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -70,6 +71,7 @@ function Footer() {
                             </svg>   
                         </button>  
                     </li>
+                    
                     <li>
                         <button>
                             <svg width="20" height="17" viewBox="0 0 20 17" fill="none" xmlns="http://www.w3.org/2000/svg">
